daa4a8029ca6382084d6bbc1ee440b5fe025328b
bspecht <bernhard@specht.net>
2016-04-19 00:47:46 +0200

    New default ports, no retries

--- lib/LWP/UserAgent/Tor.pm
+++ lib/LWP/UserAgent/Tor.pm
@@ -5,7 +5,6 @@ use warnings;
 use Carp;
 use IO::Socket::Telnet;
 use LWP::Protocol::socks;
-use Net::EmptyPort qw(empty_port);
 
 use parent 'LWP::UserAgent';
 
@@ -14,17 +13,14 @@ our $VERSION = '0.01';
 sub new {
     my ($class, %args) = @_;
 
-    my $tor_control_port = delete( $args{tor_control_port} ) // empty_port();
-    my $tor_port         = delete( $args{tor_port} )         // empty_port();
+    my $tor_control_port = delete( $args{tor_control_port} ) // 9051;
+    my $tor_port         = delete( $args{tor_port} )         // 9050;
     my $tor_ip           = delete( $args{tor_ip} )           // '127.0.0.1';
-    my $max_retries      = delete( $args{max_retries} )      // 0;
 
     my $tor_pid = _start_tor_proc($tor_ip, $tor_port, $tor_control_port);
 
     my $self = $class->SUPER::new(%args);
     $self->{_tor_pid}     = $tor_pid;
-    $self->{_max_retries} = $max_retries;
-
     $self->{_tor_socket}  = IO::Socket::Telnet->new(
         PeerAddr => $tor_ip,
         PeerPort => $tor_control_port,
@@ -49,10 +45,9 @@ sub DESTROY {
 sub _start_tor_proc {
     my ($ip, $port, $control_port) = @_;
 
-    my $pid = fork() // die "fork() failed: $!";;
+    my $pid = fork() // die "fork() failed: $!";
     if ($pid == 0) {
-        exec "tor --ControlPort $control_port --quiet --SocksPort $port";
-        #exec "tor --ControlListenaddress $ip:$port --ControlPort $control_port";
+        exec "tor --ControlListenaddress $ip:$control_port --ControlPort auto --SocksPort $port --quiet";
     }
 
     sleep 1;
@@ -65,27 +60,16 @@ sub rotate_ip {
     my ($self) = @_;
 
     my $socket      = $self->{_tor_socket};
-    my $max_retries = $self->{_max_retries};
 
-    my ($retries, $answer) = (0, q{});
-  RETRY:
+    my $answer = q{};
+
     $socket->send("AUTHENTICATE\n");
     $socket->recv($answer, 1024);
-    if ($answer ne "250 OK\r\n") {
-        if ($retries++ == $max_retries) {
-            return 0;
-        }
-        goto RETRY;
-    }
+    return 0 unless $answer eq "250 OK\r\n";
 
     $socket->send("SIGNAL NEWNYM\n");
     $socket->recv($answer, 1024);
-    if ($answer ne "250 OK\r\n") {
-        if ($retries++ == $max_retries) {
-            return 0;
-        }
-        goto RETRY;
-    }
+    return 0 unless $answer eq "250 OK\r\n";
 
     return 1;
 }
